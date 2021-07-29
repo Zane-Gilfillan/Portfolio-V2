766ec3a51ea6eba0389bdd63e385670445edd5ee
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-07-29 15:36:43 -0500

    updated about me

--- pages/about_me.js
+++ pages/about_me.js
@@ -4,6 +4,7 @@ import Footer from '../components/Footer'
 import styles from '../styles/AboutMe.module.scss'
 import Image from 'next/image'
 import proPic from '../public/img/propic.jpg'
+import next from '../public/img/next-js.svg'
 
 const about_me = () => {
     return (
@@ -14,28 +15,28 @@ const about_me = () => {
             <Nav />
 
             <div id="about-me" className={styles.about_me}>
-                <>
+                <div className={styles.showcase}>
                     <div className={styles.title}>
                         <h1 className={styles.tite_text}>zane gilfillan.</h1>
                     </div>
                     <div className={styles.profile_img}>
-                        <Image src={proPic}  />
+                        <Image src={proPic} height='500' width='400'  />
                     </div>
-                </>
+                </div>
                 
                 <div className={styles.bio_info}>
                     <div className={styles.bio_info}>
                         <p className={styles.bio_text}>i am a chicago based web developer and audio designer.</p>
                     </div>
                     <div className={styles.icon_grid}>
-                        <i className="fab fa-html5 fa-3x"></i>
-                        <i className="fab fa-css3-alt fa-3x"></i>
-                        <i className="fab fa-sass fa-3x"></i>
-                        <i className="fab fa-bootstrap fa-3x"></i>
-                        <i className="fab fa-js fa-3x"></i>
-                        <i className="fab fa-node-js fa-3x"></i>
-                        <i className="fab fa-react fa-3x"></i>
-                        <i className="fab fa-next fa-3x"></i>
+                        <i className="fab fa-html5 fa-5x"></i>
+                        <i className="fab fa-css3-alt fa-5x"></i>
+                        <i className="fab fa-sass fa-5x"></i>
+                        <i className="fab fa-js fa-5x"></i>
+                        <i className="fab fa-node-js fa-5x"></i>
+                        <i className="fab fa-react fa-5x"></i>
+                        <i className="fab fa-next fa-5x"></i>
+                        <i><Image src={next} /></i>
                     </div>
                 </div>
             </div>
