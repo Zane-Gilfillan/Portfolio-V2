5cf5d0623aad63a759b90220f828e7af7d5d70fe
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-07-29 17:53:21 -0500

    restyling contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,15 @@
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
 import styles from '../styles/Contact.module.scss'
+import 'react-quill/dist/quill.snow.css'
+import dynamic from 'next/dynamic'
+
+const QuillNoSSRWrapper = dynamic(import('react-quill'), {	
+	ssr: false,
+	
+})
+
+QuillNoSSRWrapper.displayName = 'QuillNoSSRWrapper'
 
 const contact = () => {
     return (
@@ -11,9 +20,27 @@ const contact = () => {
                 <h1>contact me.</h1>
             </div>
 
-            <div className={styles.center}>
-                <h1>zane.gilfillan@gmail.com</h1>
+            <div className={styles.container}>
+                < form className={styles.main} >
+
+                    < formGroup className={styles.inputGroup} >
+                        < label htmlFor='name'>name:</label>
+                        < input type='text' name='name' className={styles.inputField} />  
+                    </formGroup>
+
+                    < formGroup className={styles.inputGroup} >
+                        < label htmlFor='email'>email:</label>
+                        < input type='email' name='email' className={styles.inputField} />
+                    </formGroup>
+
+                    < formGroup className={styles.inputGroup} >
+                        < label htmlFor='message'></label>
+                        <QuillNoSSRWrapper type='text' name='message' className={styles.quill}  theme="snow" />
+                    </formGroup>
+                    < input type='submit' value='submit' className={styles.sub_btn}/>
+                </form >
             </div>
+            
 
             <Footer />
         </div>
